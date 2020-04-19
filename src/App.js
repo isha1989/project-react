6bea6babb4ff23cafe558d4e2d02910182663729
isha1989 <ishakalia1322@gmail.com>
2020-04-19 13:27:02 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+/*import React from 'react';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from 'react-bootstrap/Nav'
+import CarouselComponent from "./components/carousel.component";
+import Table from 'react-bootstrap/Table';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Tabs from 'react-bootstrap/Tabs';
+import TabContainer from 'react-bootstrap/TabContainer';
+import TabContent from 'react-bootstrap/TabContent';
+import TabPane from 'react-bootstrap/TabPane';*/
+
+
+
+import React from "react";
+import { Container } from "react-bootstrap";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import About from "./About";
+import NotFound from "./components/notFound";
+import Eligibility from "./eligibility";
+import FineOption from "./fineoption";
+import Hour from "./hour";
+import Registeration from "./registeration";
+import Workplacement from "./workplacement";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container>
+      <HashRouter>
+        <Switch>
+          <Route  path={"/"} component={About} />
+          <Route path={"/404"} component={NotFound} />
+        
 
+        </Switch>
+      </HashRouter>
+    </Container>
+
+  );
+
+}
 export default App;
