b8a8ce704520c298c9d15b466c99adcb57429ac3
isha1989 <ishakalia1322@gmail.com>
2020-04-21 16:54:51 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
-/*import React from 'react';
 
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import { NavigationBar } from './components/NavigationBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Nav from 'react-bootstrap/Nav'
-import CarouselComponent from "./components/carousel.component";
-import Table from 'react-bootstrap/Table';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Tabs from 'react-bootstrap/Tabs';
-import TabContainer from 'react-bootstrap/TabContainer';
-import TabContent from 'react-bootstrap/TabContent';
-import TabPane from 'react-bootstrap/TabPane';*/
 
 
 
@@ -22,15 +7,12 @@ import { Container } from "react-bootstrap";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import About from "./About";
 import NotFound from "./components/notFound";
-import Eligibility from "./eligibility";
-import FineOption from "./fineoption";
-import Hour from "./hour";
-import Registeration from "./registeration";
-import Workplacement from "./workplacement";
+
 
 function App() {
   return (
     <Container>
+      
       <HashRouter>
         <Switch>
           <Route  path={"/"} component={About} />
