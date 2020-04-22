1580c8c30b2dc61112de667be3cd71bda2564f37
isha1989 <ishakalia1322@gmail.com>
2020-04-21 19:55:57 -0600

    Update About.js

--- src/About.js
+++ src/About.js
@@ -18,7 +18,6 @@ import Workplacement from "./workplacement";
 import Registeration from "./registeration";
 
 
-
 const About = () => {
   return (
 
@@ -35,7 +34,7 @@ const About = () => {
             <Col sm={4}>
               <Nav defaultActiveKey="/home" className="flex-column">
                 <Nav.Link href="/About" >About JHSS</Nav.Link>
-                <Nav.Link href="/FineOption">About Fine option program</Nav.Link>
+                <Nav.Link href="/fineoption">About Fine option program</Nav.Link>
                 <Nav.Link href="/Hour">Hours calculator</Nav.Link>
                 <Nav.Link href="/Eligibility">Eligibility</Nav.Link>
                 <Nav.Link href="/Registeration">Registeration form</Nav.Link>
@@ -45,7 +44,7 @@ const About = () => {
             </Col>
             <Col sm={8}>
               <h1>About John Howard Society</h1>
-             
+
               <p>
                 The John Howard Society is a Canadian non-profit organization that seeks href develop understanding and
                 effective responses href the problem of crime and prison reform. It is named after John Howard, a
@@ -77,10 +76,16 @@ const About = () => {
         <Container>
           <Row>
             <Col xs={3}>
-              Follow us on FaceBook
+              <h2>Contact us:</h2>
+              Address: 1801 Toronto street, Regina, Sk.
+              <hr />
+              Click on map for directions
         </Col>
             <Col xs={9}>
+              <div class="fluid-wrapper">
 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.510025714919!2d-104.60091068426865!3d50.450226579475405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x531c1e4a2c7664bd%3A0x63542e47c549a32!2s1801%20Toronto%20St%2C%20Regina%2C%20SK%20S4P%201M7!5e0!3m2!1sen!2sca!4v1587519159370!5m2!1sen!2sca" width="830" height="200"></iframe>
+              </div>
             </Col>
           </Row>
 
