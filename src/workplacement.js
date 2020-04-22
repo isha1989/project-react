d2e5a559745cdf650aa5c08fe79b7c1d0a23aa08
isha1989 <ishakalia1322@gmail.com>
2020-04-21 19:56:19 -0600

    Update workplacement.js

--- src/workplacement.js
+++ src/workplacement.js
@@ -121,16 +121,20 @@ const Workplacement = () =>
 </Container>
   <hr />
 <Container>
-    <Row>
-        <Col xs= {3}>
-            Follow us on FaceBook
+<Row>
+            <Col xs={3}>
+              <h2>Contact us:</h2>
+              Address: 1801 Toronto street, Regina, Sk.
+              <hr />
+              Click on map for directions
         </Col>
-        <Col xs={9}>
-     
+            <Col xs={9}>
+              <div class="fluid-wrapper">
 
-
-        </Col>
-    </Row>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.510025714919!2d-104.60091068426865!3d50.450226579475405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x531c1e4a2c7664bd%3A0x63542e47c549a32!2s1801%20Toronto%20St%2C%20Regina%2C%20SK%20S4P%201M7!5e0!3m2!1sen!2sca!4v1587519159370!5m2!1sen!2sca" width="830" height="200"></iframe>
+              </div>
+            </Col>
+          </Row>
 </Container>
     
   </Router>
