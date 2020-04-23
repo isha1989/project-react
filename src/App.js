cd778afa15e6449a01d3887002858b9ca004c4ec
isha1989 <ishakalia1322@gmail.com>
2020-04-23 16:01:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import { Container } from "react-bootstrap";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import About from "./About";
 import NotFound from "./components/notFound";
+import Registeration from "./registeration";
+import Hour from "./hour";
+import Workplacement from "./workplacement";
+import FineOption from "./fineoption";
+import Eligibility from "./eligibility";
 
 
 function App() {
@@ -15,10 +20,10 @@ function App() {
       
       <HashRouter>
         <Switch>
-          <Route  path={"/"} component={About} />
+          <Route exact path={"/"} component={About} />
           <Route path={"/404"} component={NotFound} />
+         
         
-
         </Switch>
       </HashRouter>
     </Container>
